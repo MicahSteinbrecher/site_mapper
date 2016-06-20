a86d18ca760f59b0ad42873f25ce8d22ec2a7f1a
Micah <micahsteinbrecher@go.wustl.edu>
2016-06-20 18:19:49 -0400

    first commit

---
+++ crawler.py
@@ -0,0 +1,47 @@
+from HTMLParser import HTMLParser
+import requests
+from graphviz import Digraph
+
+'''
+graphviz not working use something else
+'''
+
+
+
+# create a subclass and override the handler methods
+class LinkFinder(HTMLParser):
+	def __init__(self):
+		HTMLParser.__init__(self)
+		self.out = []
+
+	def handle_starttag(self, tag, attrs):
+		if tag == 'a':
+			for name, value in attrs:
+				# If href is defined, print it.
+				if name == "href":
+					self.out.append( value)
+
+def mapSite(base, current_page, visited, site_map ):
+	if current_page not in site_map:
+		print current_page
+		r = requests.get(current_page)
+		parser = LinkFinder()
+		parser.feed(r.text)
+		site_map[current_page] = parser.out
+		for link in parser.out:
+			print('\t' + link)
+		for link in site_map[current_page]:
+			if base in link:
+				mapSite(base, link, visited, site_map )
+	return site_map
+
+if __name__ == '__main__':
+	site_map = mapSite('http://thought.so/','http://thought.so/', [], {}, )
+
+	# for key, val in site_map.iteritems():
+	# 	print 'page ' +  key + ': '
+	# 	for v in val:
+	# 		print v
+
+		
+
