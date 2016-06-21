e20293146f8daa7ac00d3484547c5199688e25d1
Micah <micahsteinbrecher@go.wustl.edu>
2016-06-21 18:17:38 -0400

    added requirements

--- crawler.py
+++ crawler.py
@@ -2,25 +2,32 @@ from HTMLParser import HTMLParser
 import requests
 import sys
 
+
+'''
+LinkFinder subclasses HTMLParser. While parsing an HTML string, <handle_starttag> appends all hyperlinks to the local list <links>
+'''
 class LinkFinder(HTMLParser):
 	def __init__(self):
 		HTMLParser.__init__(self)
-		self.out = []
+		self.links = []
 
 	def handle_starttag(self, tag, attrs):
 		if tag == 'a' or tag == 'link':
 			for name, value in attrs:
 				if name == "href":
-					self.out.append(value)
+					self.links.append(value)
 
+'''
+Takes a start url: <base>, and prints all the links found at that url. Recurses through this process for all links that fall under the same domain.
+'''
 def mapSite(base, current_page, visited, site_map):
 	if current_page not in site_map:
 		print current_page
 		r = requests.get(current_page)
 		parser = LinkFinder()
 		parser.feed(r.text)
-		site_map[current_page] = parser.out
-		for link in parser.out:
+		site_map[current_page] = parser.links
+		for link in parser.links:
 			print('\t' + link)
 		for link in site_map[current_page]:
 			if base in link:
